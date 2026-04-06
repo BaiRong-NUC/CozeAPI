04cb00e514632bd41c1a1cf41625a82a78126785
GongBaiRong <gbr@edu.nuc.email>
2026-04-06 18:55:14 +0800

    测试API KEY是否正确加载

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 import os
+from dotenv import load_dotenv
 
 # 获取工作空间列表
 def get_space_list():
     pass
+
+if __name__ == "__main__":
+    load_dotenv() 
+    COZE_API_KEY = os.environ.get("COZE_API_KEY")
+    print(f"COZE_API_KEY: {COZE_API_KEY}")
+    # # 获取工作空间列表
+    # space_list = get_space_list()
+    # print(space_list)
